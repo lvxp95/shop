c2dbf4ae22b40558705118792e9adee5bb9d61c8
lvxp95 <845554499@qq.com>
2022-11-07 22:34:36 +0800

    2022/11/07

--- src/views/layout/LayOut.vue
+++ src/views/layout/LayOut.vue
@@ -1,19 +1,19 @@
-<template>
-    <div>
 
-        <h1>
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">
             <RouterLink to="/index">角色列表</RouterLink>
             <RouterLink to="/user">用户列表</RouterLink>
-        </h1>
-
-
-
-        
-
-        <h1>侧边栏</h1>
-        <RouterView></RouterView>
-    </div>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
+
 
 <script>
 export default {
@@ -23,5 +23,13 @@ export default {
 </script>
 
 <style scoped>
-
+.el-container {
+    height: 100vh;
+}
+.el-header{
+    background: blue;
+}
+.el-aside{
+    background: black;
+}
 </style>
